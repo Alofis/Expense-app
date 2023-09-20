85c193077946aacbf82a811f29be2de32679a687
Alofis <adanailov@me.com>
2023-09-20 14:44:56 +0300

    add expnse form functionality

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import './ExpenseForm.css'
 import NewExpense from './NewExpense';
 
 const ExpenseForm = (props) => {
+    const [addExpenseForm, setAddExpenseForm] = useState(false)
     // comminicate UP between components with props + execute the prop.fn inside chield comp
     // Lifting the state up
     const saveExpenseData = (entedeExpenseData) => {
@@ -11,10 +13,20 @@ const ExpenseForm = (props) => {
         }
         console.log(expenseData);
         props.onAddExpense(expenseData);
+        setAddExpenseForm(false);
+    }
+
+    const showAddExpenseForm = () => {
+        setAddExpenseForm(true);
+    }
+
+    const hideAddExpenseForm = () => {
+        setAddExpenseForm(false);
     }
  
     return  <div className='new-expense'>
-                <NewExpense onSaveExpenseData={saveExpenseData} />
+                {!addExpenseForm && <button onClick={showAddExpenseForm}>Add a new Expense</button>}
+                {addExpenseForm && <NewExpense onSaveExpenseData={saveExpenseData} onCancel={hideAddExpenseForm}/>}
             </div>
 }
 
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -101,6 +101,7 @@ const NewExpense = (props) => {
                 </div>
             </div>
             <div className='new-expense__actions'>
+                <button type='button' onClick={props.onCancel}>Cancel</button>
                 <button type='submit'>Add expense</button>
             </div>
 
